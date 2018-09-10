6b3edd0f2725d5e0df81fceb34ba3a751d791521
elinavis@gmail.com <nf6om0sj9dj>
2018-09-10 15:26:34 -0400

    small enhancements

--- main.py
+++ main.py
@@ -1,123 +1,46 @@
-from naoqi import ALProxy, ALModule, ALBroker
-from Tkinter import *
-import time
+import json
+
+res = {}
+
+options = ["Gaze direction","Gaze behavior", "Body orientation", "Body posture", "Head orientation", "Head movement",
+           "Gestures", "Emotions", "Expressions"]
+turn_options = ["Point at", "Look for approval of", "Show something to participant", "No action at current turn", "Additional input"]
+
+for exp_num in range(1,12):
+    for round in [0,1]:
+        e_file_path = "Elina"+ "_" + str(exp_num) + "_" + str(round)+ ".json"
+        j_file_path = "Julia"+ "_" + str(exp_num) + "_" + str(round)+ ".json"
+
+        e_data = json.load( open('Elina_1_0.json'))
+        j_data = json.load( open("Julia_1_0.json"))
+
+        res[exp_num] = {}
+        res[exp_num][round] = {}
+
+        for participant in ['A', 'C', 'D']:
+            res[exp_num][round][participant] = {}
+
+            for t in range(0,121, 5):
+                res[exp_num][round][participant][t] = {}
+                res[exp_num][round][participant][t]["turn of"] = e_data[participant][str(t)]["turn of"]
 
 
-IP ='192.168.0.100'
-PORT=9559
+                for op in options:
 
-####Initialize the API's
-motionProxy = ALProxy("ALMotion",IP ,PORT )
-memory = ALProxy("ALMemory", IP, PORT)
-managerProxy = ALProxy("ALBehaviorManager", IP, PORT)
+                    if e_data[participant][str(t)][op] == j_data[participant][str(t)][op]:
+                        res[exp_num][round][participant][t][op] = e_data[participant][str(t)][op]
+                    else:
+                        res[exp_num][round][participant][t][op] = e_data[participant][str(t)][op] + "," +\
+                                                                  j_data[participant][str(t)][op]
 
-
-# We need this broker to be able to construct
-# NAOqi modules and subscribe to other modules
-# The broker must stay alive until the program exists
-myBroker = ALBroker("myBroker",
-                    "0.0.0.0",  # listen to anyone
-                    0,  # find a free port and use it
-                    IP,  # parent broker IP
-                    PORT)  # parent broker port
+                for op in turn_options:
+                    if e_data[participant][str(t)][op] or j_data[participant][str(t)][op]:
+                        if e_data[participant][str(t)][op] == j_data[participant][str(t)][op]:
+                            res[exp_num][round][participant][t][op] = e_data[participant][str(t)][op]
+                        else:
+                            res[exp_num][round][participant][t][op] = e_data[participant][str(t)][op] +","+ \
+                                                                              j_data[participant][str(t)][op]
 
 
-# param to decide on the hand movement
-
-def move_hands_robot():
-    global j
-
-    if(j%2 == 0):
-        pMaxSpeedFraction = 0.35
-        pNames = ['LShoulderRoll', 'RShoulderRoll']
-        pTargetAnglesUp = [1.3265, -1.3265]  # angels to be set for the joints in radians (if more the one - list)
-        pTargetAnglesDown = [-0.3142, 0.3142]
-        for i in range(0, 3):
-            motionProxy.post.angleInterpolationWithSpeed(pNames, pTargetAnglesUp, pMaxSpeedFraction)
-            motionProxy.post.angleInterpolationWithSpeed(pNames, pTargetAnglesDown, pMaxSpeedFraction)
-
-    else:
-        pNames1 = ['LShoulderPitch', 'RShoulderPitch']
-        pMaxSpeedFraction = 0.2
-        pTargetAnglesPitch = [-1, -1]
-        pTargetAnglesPitchDown = [1.5, 1.5]
-
-        for i in range(0, 3):
-            motionProxy.post.angleInterpolationWithSpeed(pNames1, pTargetAnglesPitch, pMaxSpeedFraction)
-            motionProxy.post.angleInterpolationWithSpeed(pNames1, pTargetAnglesPitchDown, pMaxSpeedFraction)
-
-    j+=1
-
-
-def define_next_action(happy_ratio, conc_ratio):
-    # according to the Q map from HW2
-    if happy_ratio >= 0.7 and conc_ratio >= 0.7:
-       return 1
-    elif happy_ratio < 0.7 and conc_ratio < 0.7:
-        return 1
-    elif happy_ratio < 0.7 and conc_ratio >= 0.7:
-        return 0
-    else:
-        return 1
-
-
-
-def do_action (happy_ratio, conc_ratio):
-    action = define_next_action(happy_ratio, conc_ratio)
-    if action == 0:
-        # moving body
-        managerProxy.post.runBehavior("Stand/Emotions/Positive/Excited_2")
-        managerProxy.post.runBehavior("dialog_posture/bhv_stand_up")
-    elif action == 1:
-        move_hands_robot()
-
-
-
-def end_sad_interaction ():
-    tts.say("I believe, you don't feel like listening to the story right now.")
-    tts.say("Till next time!")
-    managerProxy.runBehavior("Stand/Emotions/Neutral/Hello_1")
-    managerProxy.runBehavior("dialog_posture/bhv_sit_down")
-
-number_tries= 1
-def child_didnt_come():
-    global number_tries
-    number_tries = number_tries + 1
-
-    if (number_tries == 4):
-        return end_sad_interaction()
-
-    #Call the child same as in the start
-    if number_tries == 2:
-        tts.post.say("Come, please come!")
-        managerProxy.runBehavior("Stand/Emotions/Neutral/Hello_1")
-
-    elif number_tries == 3:
-        managerProxy.runBehavior("Stand/Emotions/Positive/Happy_1")
-        tts.say("It is going to be fun, come join me!")
-
-
-
-
-def start_robot():
-
-    global number_tries
-    number_tries = 1
-    managerProxy.runBehavior("dialog_posture/bhv_stand_up")
-
-    # managerProxy.runBehavior("dialog_posture/bhv_sit_down")
-    managerProxy.runBehavior("Stand/Emotions/Neutral/AskForAttention_1")  # whisle
-    # managerProxy.runBehavior("dialog_posture/bhv_stand_up")
-
-    #stand and call child
-    managerProxy.runBehavior("Stand/Emotions/Neutral/AskForAttention_2")
-    tts.say("Come, I gonna tell you a story!")
-
-
-def end_story():
-    tts.say("I had so much fun with you! I hope you enjoyed the story. Bye Bye")
-
-    managerProxy.runBehavior("Stand/Emotions/Neutral/Hello_1")
-    managerProxy.runBehavior("dialog_posture/bhv_sit_down")
-
+print res
 
