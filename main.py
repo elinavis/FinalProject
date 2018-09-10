020e10cf1bcf8f8183364a0c66a665bcbdffb7f0
elinavis@gmail.com <nf6om0sj9dj>
2018-09-10 16:44:58 -0400

    first main version

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ options = ["Gaze direction","Gaze behavior", "Body orientation", "Body posture",
            "Gestures", "Emotions", "Expressions"]
 turn_options = ["Point at", "Look for approval of", "Show something to participant", "No action at current turn", "Additional input"]
 
-for exp_num in range(1,2):
+for exp_num in range(1,2): #todo 12
+    res[exp_num] = {}
     for round in [0,1]:
         e_file_path = "Elina"+ "_" + str(exp_num) + "_" + str(round)+ ".json"
         j_file_path = "Julia"+ "_" + str(exp_num) + "_" + str(round)+ ".json"
@@ -14,13 +15,12 @@ for exp_num in range(1,2):
         e_data = json.load( open(e_file_path))
         j_data = json.load( open(j_file_path))
 
-        res[exp_num] = {}
         res[exp_num][round] = {}
 
-        for participant in ['A', 'C', 'D']:
+        for participant in ['A', 'C']: #todo , 'D'
             res[exp_num][round][participant] = {}
 
-            for t in range(0,121, 5):
+            for t in range(0,20, 5): #todo 120
                 res[exp_num][round][participant][t] = {}
                 res[exp_num][round][participant][t]["turn of"] = e_data[participant][str(t)]["turn of"]
 
@@ -36,9 +36,9 @@ for exp_num in range(1,2):
                 for op in turn_options:
                     val = ""
                     if op in e_data[participant][str(t)]:
-                        val =  e_data[participant][str(t)][op]
+                        val =  str(e_data[participant][str(t)][op])
                     if op in j_data[participant][str(t)] and val != j_data[participant][str(t)][op] :
-                        val += "," + j_data[participant][str(t)][op]
+                        val += "," + str(j_data[participant][str(t)][op])
 
                     res[exp_num][round][participant][t][op] = val
 
