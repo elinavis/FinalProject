a8ba316853a630225ff78b26d938635df1774e53
elinavis@gmail.com <nf6om0sj9dj>
2018-09-10 15:59:05 -0400

    small enhancements

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ options = ["Gaze direction","Gaze behavior", "Body orientation", "Body posture",
            "Gestures", "Emotions", "Expressions"]
 turn_options = ["Point at", "Look for approval of", "Show something to participant", "No action at current turn", "Additional input"]
 
-for exp_num in range(1,12):
+for exp_num in range(1,2):
     for round in [0,1]:
         e_file_path = "Elina"+ "_" + str(exp_num) + "_" + str(round)+ ".json"
         j_file_path = "Julia"+ "_" + str(exp_num) + "_" + str(round)+ ".json"
 
-        e_data = json.load( open('Elina_1_0.json'))
-        j_data = json.load( open("Julia_1_0.json"))
+        e_data = json.load( open(e_file_path))
+        j_data = json.load( open(j_file_path))
 
         res[exp_num] = {}
         res[exp_num][round] = {}
@@ -34,13 +34,13 @@ for exp_num in range(1,12):
                                                                   j_data[participant][str(t)][op]
 
                 for op in turn_options:
-                    if e_data[participant][str(t)][op] or j_data[participant][str(t)][op]:
-                        if e_data[participant][str(t)][op] == j_data[participant][str(t)][op]:
-                            res[exp_num][round][participant][t][op] = e_data[participant][str(t)][op]
-                        else:
-                            res[exp_num][round][participant][t][op] = e_data[participant][str(t)][op] +","+ \
-                                                                              j_data[participant][str(t)][op]
+                    val = ""
+                    if op in e_data[participant][str(t)]:
+                        val =  e_data[participant][str(t)][op]
+                    if op in j_data[participant][str(t)] and val != j_data[participant][str(t)][op] :
+                        val += "," + j_data[participant][str(t)][op]
 
+                    res[exp_num][round][participant][t][op] = val
 
 print res
 
