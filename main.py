4905f45f2d809ace25ae5d07ddbec8c79059b141
elinavis@gmail.com <nf6om0sj9dj>
2018-10-13 18:21:40 -0400

    updates according to the new constraints output creation

--- main.py
+++ main.py
@@ -7,42 +7,27 @@ options = ["Gaze direction","Gaze behavior", "Body orientation", "Body posture",
            "Gestures", "Emotions", "Expressions"]
 turn_options = ["Point at", "Look for approval of", "Show something to participant", "No action at current turn", "Additional input"]
 
-for exp_num in range(1,2): #todo 12
+for exp_num in range(1,12):
+
+    if exp_num == 6:
+        continue
 
     for round in [0,1]:
-        e_file_path = "Elina"+ "_" + str(exp_num) + "_" + str(round)+ ".json"
-        j_file_path = "Julia"+ "_" + str(exp_num) + "_" + str(round)+ ".json"
+        j_file_path = "../Julia"+ "_" + str(exp_num) + "_" + str(round)+ "ACD.json"
 
-        e_data = json.load( open(e_file_path))
         j_data = json.load( open(j_file_path))
 
 
-        for participant in ['A', 'C']: #todo , 'D'
+        for participant in ['A', 'C', 'D']:
 
-            for t in range(0, 20, 5): #todo 120
+            for key, item in j_data[participant].iteritems():
 
-                current_res = {"Exp_num": exp_num,
-                               "Round": round,
-                               "Participant": participant,
-                               "Time": t,
-                               "Turn of": e_data[participant][str(t)]["turn of"]}
+                item["exp_num"] = exp_num
+                item["round"] = round
+                item["participant"] = participant
 
-                for op in options:
-                    if e_data[participant][str(t)][op] == j_data[participant][str(t)][op]:
-                        current_res[op] = e_data[participant][str(t)][op]
-                    else:
-                        current_res[op] = e_data[participant][str(t)][op] + "," + j_data[participant][str(t)][op]
 
-                for op in turn_options:
-                    val = ""
-                    if op in e_data[participant][str(t)]:
-                        val =  str(e_data[participant][str(t)][op])
-                    if op in j_data[participant][str(t)] and val != str(j_data[participant][str(t)][op]) :
-                        val += "," + str(j_data[participant][str(t)][op])
-
-                    current_res[op] = val
-
-                res.append(current_res)
+                res.append(item)
 
 res_df= pd.DataFrame.from_dict(res)
 writer = pd.ExcelWriter('output.xlsx')
